3b6a2f35544c390ebbaff1de8fcb4ec72b3b1e13
Yuri Kaspervich <ykas.gg@gmail.com>
2016-02-28 22:58:43 +0300

    Minor refactoring of Traver#create

--- lib/traver.rb
+++ lib/traver.rb
@@ -3,20 +3,26 @@ require "traver/version"
 module Traver
   def self.create(options)
     if options.is_a?(Symbol)
-      klass = Object.const_get(options.to_s.capitalize)
-      
-      klass.new
+      create_bare_object(options)
     elsif options.is_a?(Hash)
-      class_name, params = options.first
-      
-      klass = Object.const_get(class_name.to_s.capitalize)
-      object = klass.new
-      
-      params.each do |k, v|
-        object.public_send("#{k}=", v)
-      end
-      
-      object
+      create_object_with_attributes(*options.first)
     end
+  end
+  
+  def self.create_bare_object(class_name)
+    klass = Object.const_get(class_name.to_s.capitalize)
+    
+    klass.new
+  end
+  
+  def self.create_object_with_attributes(class_name, attributes)
+    klass = Object.const_get(class_name.to_s.capitalize)
+    object = klass.new
+    
+    attributes.each do |k, v|
+      object.public_send("#{k}=", v)
+    end
+    
+    object
   end
 end
