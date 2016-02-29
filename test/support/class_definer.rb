4c634c72608f3fcfad858b33fbe9dc15d08f3d58
Yuri Kaspervich <ykas.gg@gmail.com>
2016-02-29 11:04:28 +0300

    Move ClassDefiner tests inside its file
    ClassDefiner tests are not belong to the gem test suite

--- test/support/class_definer.rb
+++ test/support/class_definer.rb
@@ -29,3 +29,41 @@ class ClassDefiner
     Object.send(:remove_const, class_name.to_s.capitalize)
   end
 end
+
+if __FILE__ == $0
+  require "bundler/setup"
+  require "minitest/autorun"
+  require "support/class_definer"
+
+  class ClassDefinerTest < Minitest::Test
+    def test_defines_class
+      class_definer = ClassDefiner.new
+    
+      class_definer.define_class(:blog)
+    
+      assert defined?(Blog)
+    
+      Object.send(:remove_const, "Blog")
+    end
+
+    def test_undefines_class
+      Object.const_set("Blog", Class.new)
+      class_definer = ClassDefiner.new
+
+      class_definer.undefine_class(:blog)
+    
+      refute defined?(Blog)
+    end
+  
+    def test_undefine_defined_classes
+      class_definer = ClassDefiner.new
+    
+      class_definer.define_class(:blog)
+      class_definer.define_class(:post)
+      class_definer.undefine_all_classes
+    
+      refute defined?(Blog)
+      refute defined?(Post)
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "traver"
 
+require "bundler/setup"
 require "minitest/autorun"
 require "support/class_definer_helper"
 
