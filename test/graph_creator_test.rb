008d2a0344441ffc41cf6ed879db65a25d9b90af
Yuri Kaspervich <ykas.gg@gmail.com>
2016-03-14 00:49:29 +0300

    Refactor GraphCreatorTest

--- test/graph_creator_test.rb
+++ test/graph_creator_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 class GraphCreatorTest < TraverTest
+  attr_reader :factory_definer, :object_persister, :object_creator
+  
+  def setup
+    super
+    
+    @factory_definer = FactoryDefiner.new
+    @object_persister = PoroObjectPersister.new
+    @object_creator = ObjectCreator.new(:blog,  { title: "Hello" }, factory_definer, object_persister)
+  end
+  
   def test_create_graph
     define_class("Blog", :title)
-    factory_definer = FactoryDefiner.new
-    object_persister = PoroObjectPersister.new
-    object_creator = ObjectCreator.new(:blog,  { title: "Hello" }, factory_definer, object_persister)
     graph_creator = GraphCreator.new(object_creator)
     
     graph_creator.create_graph
