56c80ad6e98bfcfbd7e0becce2b058a86e8b732c
Yuri Kaspervich <ykas.gg@gmail.com>
2016-03-28 13:10:05 +0300

    Move unit tests to test/unit folder

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "traver"
-
 require "bundler/setup"
 require "minitest/autorun"
 require "support/class_definer"
 require "support/model_definer"
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "traver"
 
 class TraverTest < Minitest::Test
   include Traver
