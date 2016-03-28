1c65e166ac5f9b30ddd09e7145ed5654ec873e82
Yuri Kaspervich <ykas.gg@gmail.com>
2016-03-28 11:10:35 +0300

    Report object errors if unable to save

--- lib/traver/object_persisters/active_record_object_persister.rb
+++ lib/traver/object_persisters/active_record_object_persister.rb
@@ -1,7 +1,9 @@
 module Traver
   class ActiveRecordObjectPersister
     def persist_object(object)
-      object.save
+      unless object.save
+        raise "Unable to save #{object.class}: #{object.errors.full_messages}"
+      end
     end
   end
 end
